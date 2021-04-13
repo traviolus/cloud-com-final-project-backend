f21e3e1ea91e852f220b72a9bb6354d8e416c2ff
traviolus <david.tumcharoen@gmail.com>
2021-04-13 20:48:39 +0700

    remove idle status from print tasks

--- project/prints/models.py
+++ project/prints/models.py
@@ -3,12 +3,10 @@ from users.models import User
 
 
 class PrintTask(models.Model):
-    IDLE = 0
     QUEUING = 1
     PRINTING = 2
     DONE = 3
     STATUS_CHOICES = [
-        (IDLE, 'Idle'),
         (QUEUING, 'Queuing'),
         (PRINTING, 'Printing'),
         (DONE, 'Done')
@@ -17,7 +15,7 @@ class PrintTask(models.Model):
     task_id = models.AutoField(primary_key=True)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='tasks')
     file_url = models.CharField(max_length=255)
-    status = models.IntegerField(choices=STATUS_CHOICES, default=IDLE)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=QUEUING)
 
     def __str__(self):
         return f'{self.task_id}: {self.owner} [{self.get_status_display()}]'
